a7e6eec46bd28f497f2871d9f3154df5b59522ea
V-jin <nice_apple@naver.com>
2026-03-13 11:25:19 +0900

    docs: update README and app code

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def index():
 def home():
     return "Welcome to the HOME page!"
 
+@app.route("/course")
+def course():
+    return "2026학년도 1학기 오픈소스 수업 테스트 페이지"
+
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port=5000, debug=True)
